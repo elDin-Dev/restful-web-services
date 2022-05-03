25523f6ed8097e0e8d441ea55967737acf92c6b8
sandina <alejandro.sandin@roche.com>
2022-05-04 00:50:27 +0200

    Initial commit

---
+++ src/main/java/com/sandin/learning/rest/webservices/restfulwebservices/infrastructure/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.sandin.learning.rest.webservices.restfulwebservices.infrastructure;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message= message;
+    }
+
+    @Override
+    public String toString() {
+        return "HeelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/sandin/learning/rest/webservices/restfulwebservices/infrastructure/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.sandin.learning.rest.webservices.restfulwebservices.infrastructure;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "hello-world")
+    public String helloWorld(){
+        return "Hello world!";
+    }
+    @GetMapping(path = "hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello world!");
+    }
+}
